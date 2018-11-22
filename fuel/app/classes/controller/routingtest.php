41d7ddcf18e9f974c4bc18c8a54c3178f59a5314
SUNLOFT\jnumagami <numagamijn@gamil.com>
2018-11-22 17:29:31 +0900

    セクション：ルート終了

---
+++ fuel/app/classes/controller/routingtest.php
@@ -0,0 +1,12 @@
+<?php
+
+	class Controller_RoutingTest extends Controller
+	{
+		public function router($resource, $arguments)
+		{
+			//ルート情報を表示
+			Debug::dump($this->request->route);
+			// 名前付きパラメータの一覧を表示
+			Debug::dump($this->params());
+		}
+	}
